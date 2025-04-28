0769e707304c1ccccae4ef13bbe2c75dfd9c86e0
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-04-28 20:50:11 +0300

    Добавил ссылки на Allure отчет и Jenkins

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -30,24 +30,7 @@ public class TestBase {
                 "enableVideo", true
         ));
 
-        // Добавляем необходимые аргументы для запуска Chrome
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments(
-                "--headless", // запуск в headless-режиме
-                "--no-sandbox", // предотвращает ошибки, связанные с правами
-                "--disable-dev-shm-usage", // использование памяти в контейнерах
-                "--disable-gpu", // отключение GPU
-                "--remote-allow-origins=*",
-                "--window-size=1920,1080",
-                "--disable-software-rasterizer", // отключение программного рендеринга
-                "--remote-debugging-port=9222", // удалённая отладка
-                "--disable-features=VizDisplayCompositor", // отключение графических функций
-                "--disable-extensions", // отключение расширений
-                "--disable-infobars", // отключение информационных панелей
-                "--start-maximized" // запуск с максимизированным окном
-        );
 
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
         Configuration.browserCapabilities = capabilities;
 
@@ -82,12 +65,14 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
+
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+
         Selenide.closeWebDriver();
     }
 }
