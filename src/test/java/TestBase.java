a274cfa9c76392ba44d86da01948323539a121d3
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-04-29 22:12:10 +0300

    Добавил ссылки на Allure отчет и Jenkins

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -41,9 +41,9 @@ public class TestBase {
     }
 
     private static String getRemoteWebDriverUrl() {
-        String remote = System.getProperty("remoteUrl");
-        if (remote == null || remote.isEmpty()) {
-            return null; // или выбросить исключение, если обязательный параметр
+        String remote = System.getProperty("remote", "");
+        if (remote.isEmpty()) {
+            return (remote);
         }
 
         String user = System.getenv("SELENOID_USER");
