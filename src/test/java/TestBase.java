25e7641348eb05551a1aa42a5115b3703d1bbc49
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-05-02 14:35:29 +0300

    Добавил ссылки на Allure отчет и Jenkins

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -12,50 +12,35 @@ import java.util.Map;
 
 public class TestBase {
 
+
     @BeforeAll
     static void setUpBrowserConfiguration() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "wd/hub";
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = System.getProperty("loadStrategy", "eager");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://centicore.ru");
 
-        String remoteUrl = getRemoteWebDriverUrl();
-        if (remoteUrl != null) {
-            Configuration.remote = remoteUrl;
-
-        } else {
-            Configuration.remote = null;
-
-        }
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.of(
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
         Configuration.browserCapabilities = capabilities;
+
     }
 
-    private static String getRemoteWebDriverUrl() {
-        String useRemote = System.getProperty("remote", ""); // Если -Dremote передан
-        if (useRemote.isEmpty()) {
-            return null; // Локальный запуск
-        }
 
-        String user = System.getenv("SELENOID_USER");
-        String password = System.getenv("SELENOID_PASSWORD");
-        String host = System.getProperty("wd", "selenoid.autotests.cloud");
-
-
-
-        return String.format("https://%s:%s@%s/wd/hub", user, password, host);
-    }
 
     @BeforeEach
     void addSelenideLogger() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
 
     @AfterEach
     void addAttachments() {
