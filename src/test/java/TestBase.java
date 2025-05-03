63a55e69da0721846a80e4c95ec34a728821ae63
Vitaly Kuzmin <vitalyqa34@gmail.com>
2025-05-03 18:01:32 +0300

    Добавил ссылки на Allure отчет и Jenkins

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -15,7 +15,7 @@ import static java.lang.String.format;
 public class TestBase {
     @BeforeAll
     static void setUpBrowserConfiguration() {
-      //  String getWdHost = format("https://user1:1234@%s/wd/hub", System.getProperty("wd", "selenoid.autotests.cloud"));
+        String getWdHost = format("https://user1:1234@%s/wd/hub", System.getProperty("wd", "selenoid.autotests.cloud"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         Configuration.browser = System.getProperty("browser", "chrome");
@@ -23,7 +23,7 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = System.getProperty("loadStrategy", "eager");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://centicore.ru");
-      //  Configuration.remote = getWdHost;
+        Configuration.remote = getWdHost;
         Configuration.browserCapabilities = capabilities;
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
